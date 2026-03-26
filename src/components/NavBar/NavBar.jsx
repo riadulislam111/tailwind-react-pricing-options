2353f7d442cd93468a216233835a7ed9a674afc7
riadulislam.bd.pabna@gmail.com <your_github_email@gmail.com>
2026-03-26 16:10:20 +0600

    added all file updates

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from './Link';
+import { Menu, X } from 'lucide-react';
 
 
 
@@ -32,11 +33,25 @@ const navigationData = [
 ];
 
 const NavBar = () => {
+ const [open, setOpen] = useState(false);
+ const links = navigationData.map(route => <Link key={route.id} route={route}></Link>)
+
     return (
-        <nav>
-<ul className='flex'>
+        <nav className='flex justify-between mx-10'>
+            <span className='flex' onClick={() => setOpen(!open)}>
+                {
+                    open ? <X className='md:hidden'></X> : <Menu className='md:hidden'></Menu>
+                }
+                
+<ul className='md:hidden'>
+    {links}
+</ul>
+
+            <h3 className='ml-4'>My Navbar</h3>
+            </span>
+<ul className='hidden md:flex'>
     {
-    navigationData.map(route => <Link key={route.id} route={route}></Link>)
+    links
 }
 </ul>
 
@@ -55,6 +70,8 @@ const NavBar = () => {
                 <li className='mr-10'><a href="/">About</a></li>
                 <li className='mr-10'><a href="/">Blog</a></li>
             </ul> */}
+
+            <button>Sign In</button>
         </nav>
     );
 };
